79eff8c8d8d14da1dcc639ede00b202d449efa12
zhouwenya <zhouwenya@actionsky.com>
2021-02-16 21:40:41 +0800

    config todo status

--- custom-post.el
+++ custom-post.el
@@ -2,46 +2,25 @@
 (setq org-stuck-projects
   '("TODO={.+}/-DONE" nil nil "SCHEDULED:\\|DEADLINE:"))
 
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING")))
 
-;; 设置一些 agenda 视图， 参考：http://www.mamicode.com/info-detail-2316948.html
-(setq org-agenda-custom-commands
-      '(
-        ("f" "灵光一闪" tags "NOTE")
-        ("w" . "任务安排")
-        ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
-        ("wb" "重要且不紧急的任务" tags-todo "-weekly-monthly-daily+PRIORITY=\"B\"")
-        ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
-        ("W" "Weekly Review"
-         ((stuck "") ;; review stuck projects as designated by org-stuck-projects
-          (tags-todo "project")
-          (tags-todo "daily")
-          (tags-todo "weekly")
-          (tags-todo "school")
-          (tags-todo "code")
-          (tags-todo "theory")
+      org-todo-keyword-faces (quote (
+                                     ("NEXT" :foreground "blue" :weight bold)
+                                     ("WAITING" . warning)
+                                     ("HOLD" :foreground "magenta" :weight bold)
           ))
-        ("g" "Get Things Done (GTD)" ;; ref: https://github.com/rougier/emacs-gtd
-         ((agenda ""
-                  ((org-agenda-skip-function
-                    '(org-agenda-skip-entry-if 'deadline))
-                   (org-deadline-warning-days 0)))
-          (todo "NEXT"
-                ((org-agenda-skip-function
-                  '(org-agenda-skip-entry-if 'deadline))
-                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
-                 (org-agenda-overriding-header "\nTasks\n")))
-          (agenda nil
-                  ((org-agenda-entry-types '(:deadline))
-                   (org-agenda-format-date "")
-                   (org-deadline-warning-days 7)
-                   (org-agenda-skip-function
-                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
-                   (org-agenda-overriding-header "\nDeadlines")))
-          (tags-todo "inbox"
-                     ((org-agenda-prefix-format "  %?-12t% s")
-                      (org-agenda-overriding-header "\nInbox\n")))
-          (tags "CLOSED>=\"<today>\""
-                ((org-agenda-overriding-header "\nCompleted today\n"))))
+
+      org-use-fast-todo-selection t
+      org-treat-S-cursor-todo-selection-as-state-change nil
+      org-todo-state-tags-triggers      (quote (("CANCELLED" ("CANCELLED" . t))
+                                                ("WAITING" ("WAITING" . t))
+                                                ("HOLD" ("WAITING") ("HOLD" . t))
+                                                (done ("WAITING") ("HOLD"))
+                                                ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+                                                ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+                                                ("DONE" ("WAITING") ("CANCELLED") ("HOLD"))))
          )
         ))
 
