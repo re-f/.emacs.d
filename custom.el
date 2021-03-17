61ec87e110656adef19ccb38abab3d9ab63653ec
zhouwenya <zhouwenya@actionsky.com>
2021-03-17 19:49:36 +0800

    老年人的象征之一：需要用更大的字了🤣

--- custom.el
+++ custom.el
@@ -35,7 +35,7 @@
            when (font-installed-p font)
            return (set-face-attribute 'default nil
                                       :font font
-                                      :height (cond (sys/mac-x-p 130)
+                                      :height (cond (sys/mac-x-p 170)
                                                     (sys/win32p 110)
                                                     (t 100))))
 
