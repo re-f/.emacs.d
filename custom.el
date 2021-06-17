94ed7a909eb05720b2fea7f6d0a0aa184f2cc47d
ref <zhou.erica.w@gmail.com>
2021-06-17 17:36:17 +0800

    customize org-sidebar

--- custom.el
+++ custom.el
@@ -79,8 +79,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(centaur-package-archives 'netease)
+ '(centaur-theme 'default)
+ '(org-sidebar-default-fns
+   '(org-sidebar--upcoming-items my/org-sidebar org-sidebar--todo-items )))
 
+(defun my/org-sidebar (source-buffer)
+  "Import TODO."
+  (let ((display-buffer
+         (generate-new-buffer (format "org-sidebar<%s>" (buffer-name source-buffer))))
+        (title (concat "Import items in: " (buffer-name source-buffer))))
+    (with-current-buffer display-buffer
+      (setf org-sidebar-source-buffer source-buffer))
+    (save-window-excursion
+      ;; `org-ql-search' displays the buffer, but we don't want to do that here.
+      (org-ql-search source-buffer
+        '(and (priority > "B")
+              (not (done)))
+        :narrow t :sort 'date
+        :super-groups '((:auto-planning))
+        :buffer display-buffer
+        :title title))
+    display-buffer))
+    
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
