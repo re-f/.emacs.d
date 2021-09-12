94f5e057aeb3c07d0b9a6a0ea553c5aa059c2349
Ref <z.ref@outlook.com>
2021-09-12 22:09:34 +0800

    move all function into independent file

---
+++ lisp/init-ref.el
@@ -0,0 +1,31 @@
+;; init-rel.el --- Initialize Ref configurations.	-*- lexical-binding: t -*-
+
+(defun ref/org-timeS-less-p (x y)
+  "比较两个org-mode时间格式的时间大小，其中x，y为string类型"
+  (org-time-less-p (org-timestamp-to-time(org-timestamp-from-string x))
+                   (org-timestamp-to-time (org-timestamp-from-string y)))
+  )
+
+
+(defun ref/create-org-file ()
+      "Create an org file in org-directory"
+      (interactive)
+      (let ((name (read-string "Filename: ")))
+        (expand-file-name (format "%s.org"
+                                  name) org-directory)))
+
+
+
+;; 快捷键习惯用的函数
+(defun ref/newline-return ()
+  "append (open) a new line below the current line, and execute return"
+  (interactive)
+    (move-end-of-line 1)
+  (org-newline-and-indent))
+(defun ref/newline-meta-return ()
+  "append (open) a new line below the current line, org-meta-return"
+  (interactive)
+    (move-end-of-line 1)
+  (org-meta-return))
+  
+(provide 'init-ref)
