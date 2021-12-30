402c59240f7f6342941d2ac8e194a7124800ccfa
Ref <z.ref@outlook.com>
2021-12-30 20:19:58 +0800

    add personal setting

--- custom.el
+++ custom.el
@@ -8,11 +8,13 @@
 (setq centaur-logo nil)                        ; Logo file or nil (official logo)
 (setq centaur-full-name "ref")           ; User full name
 (setq centaur-mail-address "z.ref@outlook.com")   ; Email address
-;; (setq centaur-proxy "127.0.0.1:1080")          ; Network proxy
+
+(setq centaur-proxy "127.0.0.1:7890")          ; HTTP/HTTPS proxy
+(setq centaur-socks-proxy "127.0.0.1:7890")    ; SOCKS proxy
 ;; (setq centaur-server nil)                      ; Enable `server-mode' or not: t or nil
 ;; (setq centaur-icon nil)                        ; Display icons or not: t or nil
-(setq centaur-package-archives 'netease)   ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
-;; (setq centaur-theme 'light)                    ; Color theme: auto, random, default, classic, colorful, dark, light, day or night
+(setq centaur-package-archives 'melpa); Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
+(setq centaur-theme 'dark)                    ; Color theme: auto, random, default, classic, colorful, dark, light, day or night
 ;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
 ;; (setq centaur-restore-frame-geometry nil)      ; Restore the frame's geometry at startup: t or nil
 ;; (setq centaur-lsp 'eglot)                      ; Set LSP client: lsp-mode, eglot or nil
@@ -79,11 +81,54 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(centaur-package-archives 'netease)
- '(centaur-theme 'default)
+ '(ansi-color-names-vector
+   ["#1D252C" "#D95468" "#8BD49C" "#EBBF83" "#5EC4FF" "#E27E8D" "#70E1E8" "#A0B3C5"])
+ '(centaur-completion-style 'minibuffer)
+ '(centaur-package-archives 'melpa)
+ '(centaur-theme 'warm)
+ '(custom-safe-themes
+   '("c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" default))
+ '(fci-rule-color "#56697A")
+ '(jdee-db-active-breakpoint-face-colors (cons "#10151C" "#5EC4FF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#10151C" "#8BD49C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#10151C" "#41505E"))
+ '(objed-cursor-color "#D95468")
  '(org-id-link-to-org-use-id t)
+ '(org-modules '(org-crypt org-habit org-id))
  '(org-sidebar-default-fns
-   '(org-sidebar--upcoming-items my/org-sidebar org-sidebar--todo-items )))
+   '(org-sidebar--upcoming-items my/org-sidebar org-sidebar--todo-items))
+ '(org-use-property-inheritance '("CATEGORY"))
+ '(paradox-github-token t)
+ '(pdf-view-midnight-colors (cons "#A0B3C5" "#1D252C") t)
+ '(pyim-dicts
+   '((:name "计算机词汇大全" :file "~/.emacs.d/pyim/dict/计算机词汇大全.pyim")
+     (:name "开发词库" :file "~/.emacs.d/pyim/dict/开发大神专用词库.pyim")
+     (:name "全国县及县以上行政区划地名" :file "~/.emacs.d/pyim/dict/全国县及县以上行政区划地名.pyim")
+     (:name "中国高等院校（大学）大全" :file "~/.emacs.d/pyim/dict/中国高等院校（大学）大全.pyim")))
+ '(rustic-ansi-faces
+   ["#1D252C" "#D95468" "#8BD49C" "#EBBF83" "#5EC4FF" "#E27E8D" "#70E1E8" "#A0B3C5"])
+ '(vc-annotate-background "#1D252C")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#8BD49C")
+    (cons 40 "#abcd93")
+    (cons 60 "#cbc68b")
+    (cons 80 "#EBBF83")
+    (cons 100 "#e5ae6f")
+    (cons 120 "#df9e5b")
+    (cons 140 "#D98E48")
+    (cons 160 "#dc885f")
+    (cons 180 "#df8376")
+    (cons 200 "#E27E8D")
+    (cons 220 "#df7080")
+    (cons 240 "#dc6274")
+    (cons 260 "#D95468")
+    (cons 280 "#b35365")
+    (cons 300 "#8d5163")
+    (cons 320 "#675160")
+    (cons 340 "#56697A")
+    (cons 360 "#56697A")))
+ '(vc-annotate-very-old-color nil))
 
 (defun my/org-sidebar (source-buffer)
   "Import TODO."
@@ -102,19 +147,67 @@
         :buffer display-buffer
         :title title))
     display-buffer))
-    
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
+ '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 1.0))))
+ '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
+ '(dashboard-heading ((t (:inherit (font-lock-string-face bold)))))
+ '(diff-hl-change ((t (:foreground "#51afef" :background nil))))
+ '(diff-hl-delete ((t (:inherit diff-removed :background nil))))
+ '(diff-hl-insert ((t (:inherit diff-added :background nil))))
+ '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
+ '(flycheck-posframe-background-face ((t (:inherit tooltip))))
+ '(flycheck-posframe-border-face ((t (:inherit font-lock-comment-face))))
+ '(flycheck-posframe-face ((t (:foreground "#859900"))))
+ '(flycheck-posframe-info-face ((t (:foreground "#859900"))))
+ '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(ivy-minibuffer-match-face-1 ((t (:foreground "#83898d"))))
+ '(ivy-posframe ((t (:inherit tooltip))))
+ '(ivy-posframe-border ((t (:background "#96A7A9"))))
+ '(lsp-headerline-breadcrumb-path-error-face ((t :underline (:style wave :color "#ff6c6b") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-hint-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-info-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-path-warning-face ((t :underline (:style wave :color "#ECBE7B") :inherit lsp-headerline-breadcrumb-path-face)))
+ '(lsp-headerline-breadcrumb-symbols-error-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ff6c6b"))))
+ '(lsp-headerline-breadcrumb-symbols-hint-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
+ '(lsp-headerline-breadcrumb-symbols-info-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
+ '(lsp-headerline-breadcrumb-symbols-warning-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ECBE7B"))))
+ '(lsp-ui-sideline-code-action ((t (:inherit warning))))
+ '(macrostep-expansion-highlight-face ((t (:inherit tooltip :extend t))))
  '(org-done ((t (:inherit org-headline-done :strike-through nil :weight bold))))
+ '(org-ellipsis ((t (:foreground nil))))
  '(org-headline-done ((t (:foreground "#5B6268" :strike-through nil))))
- )
+ '(org-pomodoro-mode-line ((t (:inherit warning))))
+ '(org-pomodoro-mode-line-break ((t (:inherit success))))
+ '(org-pomodoro-mode-line-overtime ((t (:inherit error))))
+ '(pulse-highlight-face ((t (:inherit region))))
+ '(pulse-highlight-start-face ((t (:inherit region))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold)))))
+ '(transient-posframe ((t (:inherit tooltip))))
+ '(transient-posframe-border ((t (:background "#96A7A9"))))
+ '(which-key-posframe ((t (:inherit tooltip))))
+ '(which-key-posframe-border ((t (:background "#96A7A9"))))
+ '(ztreep-arrow-face ((t (:inherit font-lock-comment-face))))
+ '(ztreep-diff-header-face ((t (:inherit (diff-header bold)))))
+ '(ztreep-diff-header-small-face ((t (:inherit diff-file-header))))
+ '(ztreep-diff-model-add-face ((t (:inherit diff-nonexistent))))
+ '(ztreep-diff-model-diff-face ((t (:inherit diff-removed))))
+ '(ztreep-diff-model-ignored-face ((t (:inherit font-lock-doc-face :strike-through t))))
+ '(ztreep-diff-model-normal-face ((t (:inherit font-lock-doc-face))))
+ '(ztreep-expand-sign-face ((t (:inherit font-lock-function-name-face))))
+ '(ztreep-header-face ((t (:inherit diff-header))))
+ '(ztreep-leaf-face ((t (:inherit diff-index))))
+ '(ztreep-node-face ((t (:inherit font-lock-variable-name-face)))))
 
 ;; ref: https://yiming.dev/blog/2018/03/02/my-org-refile-workflow/
 ;; This config lets org-refile searches all the opened org mode files and fetches all the headings in them.
-;; 
+;;
 
 
 ;; 尝试做加密，但是失败了
